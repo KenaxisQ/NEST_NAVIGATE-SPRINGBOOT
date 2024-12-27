5d8e1321bd0c9b5c0c8852bf7645386aae32b7a2
KenaxisQ <kenaxisQ@gmail.com>
2024-12-28 00:48:02 +0530

    Update V2__Insert_amenities.sql
    Added Icons

--- src/main/resources/db/migration/V2__Insert_amenities.sql
+++ src/main/resources/db/migration/V2__Insert_amenities.sql
@@ -1,14 +1,14 @@
 INSERT INTO amenity (id, name, category, sub_category) VALUES
 -- Land Amenities
-('1', 'Electricity Connection', 'LAND', 'Amenities'),
-('2', 'Water Supply', 'LAND', 'Amenities'),
-('3', 'Sewage Connection', 'LAND', 'Amenities'),
+('1', 'Electricity Connection', 'LAND', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Electricity.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T174134Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=f6549b03c28ef04bd0313cd5ae6bf03a40d58d1e7e8ccc5ccf26665b98c64f45
+('2', 'Water Supply', 'LAND', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Water%2BTap.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T174558Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=b45620dff6086422ba59332efa90a3d08f6b6c7fecf2d56e455f10c2c953b024
+('3', 'Sewage Connection', 'LAND', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Sewage.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T174622Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=4d813ab80ac84c1fec9ba228232119c4701cfedd0393d4ac6bd7ceea3e976b62
 ('4', 'Fencing', 'LAND', 'Amenities'),
-('5', 'Road Access', 'LAND', 'Amenities'),
+('5', 'Road Access', 'LAND', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Road.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T175633Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=8d18d8c838096edb07587a23a95ef0730a6386f2c62dae7a57c6b6eecc328df7
 ('6', 'Gated Access', 'LAND', 'Amenities'),
 ('7', 'Irrigation', 'LAND', 'Amenities'),
 ('8', 'Zoning Approval', 'LAND', 'Amenities'),
-('9', 'Drainage System', 'LAND', 'Amenities'),
+('9', 'Drainage System', 'LAND', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Drainage.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T181119Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=e03929d5805e23a7d0c678f09a029428c04b0d9e538757c46a8b0c7904d526e5
 ('10', 'Site Office', 'LAND', 'Amenities'),
 ('11', 'Landscaping', 'LAND', 'Amenities'),
 ('12', 'Borewell', 'LAND', 'Amenities'),
@@ -21,22 +21,22 @@ INSERT INTO amenity (id, name, category, sub_category) VALUES
 ('19', 'Taxi Stand', 'LAND', 'Near By Services'),
 ('20', 'Natural Park', 'LAND', 'Near By Services'),
 ('21', 'Shopping Complex', 'LAND', 'Near By Services'),
-('22', 'Hospital', 'LAND', 'Near By Services'),
-('23', 'Bus Stand', 'LAND', 'Near By Services'),
-('24', 'Railway Station', 'LAND', 'Near By Services'),
+('22', 'Hospital', 'LAND', 'Near By Services'),https://innoida.utho.io/nestnavigate/Icon/Hospital.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T183643Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=0ef0cad4a8373274476553192e750e9ecde9659c6223aec13c68666ca180f670
+('23', 'Bus Stand', 'LAND', 'Near By Services'),https://innoida.utho.io/nestnavigate/Icon/Bustand.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T183735Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=568ac977ae9ee8bcb9cc5f1d5a53fbb2b9456002dc6e487fa7ff69b258a36b99
+('24', 'Railway Station', 'LAND', 'Near By Services'),https://innoida.utho.io/nestnavigate/Icon/Railway%2BStation.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T184333Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=7e66098ab4939e662d6f975493fbe4f553394dfe3909dbe935a863cec6882771
 -- Residential Amenities
-('25', 'Gym', 'RESIDENTIAL', 'Amenities'),
-('26', 'Swimming Pool', 'RESIDENTIAL', 'Amenities'),
-('27', 'Parking', 'RESIDENTIAL', 'Amenities'),
-('28', 'Elevator', 'RESIDENTIAL', 'Amenities'),
-('29', 'Security', 'RESIDENTIAL', 'Amenities'),
+('25', 'Gym', 'RESIDENTIAL', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Gym.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T184434Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=ce825794e10c1a5bd3a7c0a3801209abdfe1cb75d93b304c82312c6a616b65fd
+('26', 'Swimming Pool', 'RESIDENTIAL', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/SwimmingPool.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T184456Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=ecded33be7d97dd3f5ddb8266c35930ee078ee92d4d8abab59c8b8ff19d487d9
+('27', 'Parking', 'RESIDENTIAL', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Parking.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T184522Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=6e4e3684f0b8efa4f4720d9012e7d849157a46352868433ff69b111433f89f06
+('28', 'Elevator', 'RESIDENTIAL', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Elevator.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T184540Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=1d3bb3c2157b957b7b02f20095a19e1db81e49ec4b6b84da90c86135f79f4c3f
+('29', 'Security', 'RESIDENTIAL', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Security.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T184600Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=217ad11afa84e6adc9d1d69ffea7b3def76359494a923843e780ba9485d07cb3
 ('30', 'Power Backup', 'RESIDENTIAL', 'Amenities'),
-('31', 'Wi-Fi', 'RESIDENTIAL', 'Amenities'),
-('32', 'Air Conditioning', 'RESIDENTIAL', 'Amenities'),
-('33', 'Heating', 'RESIDENTIAL', 'Amenities'),
-('34', 'Balcony', 'RESIDENTIAL', 'Amenities'),
-('35', 'Garden', 'RESIDENTIAL', 'Amenities'),
-('36', 'Playground', 'RESIDENTIAL', 'Amenities'),
+('31', 'Wi-Fi', 'RESIDENTIAL', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Wi-fi.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T185258Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=ade3062bebd1df52f18d5ad6177c3e76fc5c5727b545887e314546d770b7b135
+('32', 'Air Conditioning', 'RESIDENTIAL', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/AirConditioning.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T185342Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=6a2189cc28c339b92a48fca45afa0803e65bad214bce7e7fd93e4446f62ab39d
+('33', 'Heating', 'RESIDENTIAL', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Heating.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T185612Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=f79e0aa7a0a5e4898572d0775030acee09bf72b13b280c58f5fa0ecd96d34607
+('34', 'Balcony', 'RESIDENTIAL', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Balcony.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T185714Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=78449d71656c97cc3cd972eba97985897c8ba5348c3d152854439a732758b7ac
+('35', 'Garden', 'RESIDENTIAL', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Garden.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T185820Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=a7709ac162c1d8437b38e372ecc0115e228d12038d5e8b944703d0d5f5b7b7d6
+('36', 'Playground', 'RESIDENTIAL', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/Playground.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T185839Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=3458a458a2bb14a2569fccb4189029aaa618facc8b892a74fbd266a62b240eca
 ('37', 'Community Hall', 'RESIDENTIAL', 'Amenities'),
 ('38', 'Clubhouse', 'RESIDENTIAL', 'Amenities'),
 ('39', 'Jogging Track', 'RESIDENTIAL', 'Amenities'),
@@ -45,7 +45,7 @@ INSERT INTO amenity (id, name, category, sub_category) VALUES
 ('42', 'Intercom', 'RESIDENTIAL', 'Amenities'),
 ('43', 'Maintenance Staff', 'RESIDENTIAL', 'Amenities'),
 ('44', 'Rainwater Harvesting', 'RESIDENTIAL', 'Amenities'),
-('45', 'Fire Safety', 'RESIDENTIAL', 'Amenities'),
+('45', 'Fire Safety', 'RESIDENTIAL', 'Amenities'),https://innoida.utho.io/nestnavigate/Icon/fire%2Bextinguisher.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T190502Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=9fff41177c41a4ccec8335a77acbaa613cb2d95636bb626d2b81f67912dc658b
 ('46', 'Waste Disposal', 'RESIDENTIAL', 'Amenities'),
 ('47', 'Library', 'RESIDENTIAL', 'Amenities'),
 ('48', 'Reserve Parking', 'RESIDENTIAL', 'Amenities'),
@@ -61,9 +61,9 @@ INSERT INTO amenity (id, name, category, sub_category) VALUES
 ('58', 'Financing Provided', 'RESIDENTIAL', 'Miscellaneous'),
 ('59', 'Ownership documents provided', 'RESIDENTIAL', 'Miscellaneous'),
 ('60', 'Taxi Stand', 'RESIDENTIAL', 'Near By Services'),
-('61', 'Natural Park', 'RESIDENTIAL', 'Near By Services'),
+('61', 'Natural Park', 'RESIDENTIAL', 'Near By Services'),https://innoida.utho.io/nestnavigate/Icon/Park.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T191147Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=31d0913d87e07a07f9761177aeab7f6ce4ff94d2f21946a8a6c7ca4c68f968d4
 ('62', 'Shopping Complex', 'RESIDENTIAL', 'Near By Services'),
-('63', 'Hospital', 'RESIDENTIAL', 'Near By Services'),
+('63', 'Hospital', 'RESIDENTIAL', 'Near By Services'),https://innoida.utho.io/nestnavigate/Icon/Hospital.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T191226Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=838dff36e31e8ff82c2e0865ebdd41a91be11b2749036ee772a2138a606b838c
 ('64', 'Bus Stand', 'RESIDENTIAL', 'Near By Services'),
 ('65', 'Railway Station', 'RESIDENTIAL', 'Near By Services'),
 -- Commercial Amenities
@@ -90,7 +90,7 @@ INSERT INTO amenity (id, name, category, sub_category) VALUES
 ('86', 'Natural Park', 'COMMERCIAL', 'Near By Services'),
 ('87', 'Shopping Complex', 'COMMERCIAL', 'Near By Services'),
 ('88', 'Hospital', 'COMMERCIAL', 'Near By Services'),
-('89', 'Bus Stand', 'COMMERCIAL', 'Near By Services'),
+('89', 'Bus Stand', 'COMMERCIAL', 'Near By Services'),https://innoida.utho.io/nestnavigate/Icon/Bustand.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=uthoadmin%2F20241227%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241227T191603Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=9efd3a7b5ff7e316b91629ac743b2e102f79c6e7f79f24aeb55b38675eb03da1
 ('90', 'Railway Station', 'COMMERCIAL', 'Near By Services'),
 -- PG Amenities
 ('91', 'Furnished Rooms (Bed, Table, Chair, Wardrobe)', 'PG', 'Amenities'),
